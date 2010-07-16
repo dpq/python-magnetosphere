750e70622a228a6367fa1170d59fcec475b42e5f
jaffar.rumith@gmail.com <jaffar.rumith@gmail.com@4e150a12-f27b-11de-82c3-25da7dca7fdf>
2010-07-16 11:56:06 +0000

    Created a Python package instead of two separate libraries

--- install.sh
+++ install.sh
@@ -22,15 +22,15 @@ cp nssdcftp.gsfc.nasa.gov/models/geomagnetic/igrf/fortran_code/* .
 for x in `ls *.for`; do mv $x `echo $x | sed s/.for/.F/`; done
 f2c igrf_sub.F
 
-cat igrf_sub.c | sed "s^o__1.ofnm = fout^extern char *trimwhitespace(char *str)
-char prefix_dp[28] = \"$TARGET\"
-strcat(prefix_dp, fspec); o__1.ofnm = trimwhitespace(prefix_dp)^" > igrf_sub2.c
+cat igrf_sub.c | sed "s^o__1.ofnm = fout^extern char *trimwhitespace(char *str); \
+char prefix_dp[28] = \"$TARGET\"; strcat(prefix_dp, fspec); \
+o__1.ofnm = trimwhitespace(prefix_dp)^" > igrf_sub2.c
 mv igrf_sub2.c igrf_sub.c
 
 sudo mv *.dat $TARGET
-sudo python setup-igrf.py install
-sudo rm -rf build *.F bilcal_log.txt 00_readme.txt igrf_sub.c
 
 cp cxform-0.71/cxform-manual.c cxform-0.71/cxform-auto.c cxform-0.71/cxform.h .
-sudo python setup-cxform.py install
+
+sudo python setup.py install
+sudo rm -rf build *.F bilcal_log.txt 00_readme.txt igrf_sub.c bilcal.log
 sudo rm -rf build cxform-auto.c cxform-manual.c cxform.h
