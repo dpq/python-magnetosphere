806be11d550eecdd372a9d5eb763bae1990aa4b2
jaffar.rumith@gmail.com <jaffar.rumith@gmail.com@4e150a12-f27b-11de-82c3-25da7dca7fdf>
2011-02-08 21:28:55 +0000

    Moved to a newer version of IGRF-11
    This version of IGRF v11 has all coefficients incorporated into the source code, thus
    eliminating the need to keep .dat files in a special local directory.

--- rigiditymodule.c
+++ rigiditymodule.c
@@ -42,9 +42,9 @@ static PyObject *cutoff_rigidity_corrected(PyObject *self, PyObject *args) {
 static PyMethodDef RigidityMethods[] = {
 	{"cutoff450",  cutoff_rigidity450, METH_VARARGS, "cutoff(lat, lon)\n\nCalculate cutoff rigidity at 450km altitude."},
 	{"cutoff",  cutoff_rigidity, METH_VARARGS, "cutoff(lat, lon, alt)\n\nCalculate cutoff rigidity at the specified altitude (extrapolation from\
-values calculated at the 450km grid)."},
+ values calculated at the 450km grid)."},
 	{"correctedcutoff",  cutoff_rigidity_corrected, METH_VARARGS, "correctedcutoff(rig, kp, lt)\n\nCalculate corrected cutoff rigidity\
-taking into account the current Kp index value and the local time."},
+ taking into account the current Kp index value and the local time."},
 	{NULL, NULL, 0, NULL}
 };
 
